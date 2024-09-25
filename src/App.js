ed8196c3fadb6bf92530a6586133b3e0ef973c65
Vedant-Chakkarwar <100403628+Vedant-Chakkarwar@users.noreply.github.com>
2024-09-25 14:42:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Gurunath Salve K8 42461</h1>
+      <h1>Hello, welcome to my React app hosted on AWS Amplify!</h1>
     </div>
   );
 }
