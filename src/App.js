91b9c40132007869f9f0620a61e7c5dfab72d5fa
Vedant-Chakkarwar <100403628+Vedant-Chakkarwar@users.noreply.github.com>
2024-10-09 13:04:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>yYasharth Sonar</h1>
+      <h1>Yasharth Sonar</h1>
       <h1>Hello, welcome to my React app hosted on AWS Amplify!</h1>
       
     </div>
