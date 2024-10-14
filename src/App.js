b1f1accf809a2ebfc66ed29c326eb73f4674f9be
Vedant-Chakkarwar <100403628+Vedant-Chakkarwar@users.noreply.github.com>
2024-10-14 14:23:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello, welcome to my React app hosted on AWS Amplify!</h1>
-      <h2> Vedant Chakkarwar  </h2>
+      <h2> Maitree Purohit  </h2>
     </div>
   );
 }
